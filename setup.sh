6dd456ffd4fdfa71fa937a405e199061c10cfbf8
Tatsuya KAWAMURA <kawa.xxx@gmail.com>
2012-09-02 12:06:16 +0900

    add .gemrc file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOT_FILES=(.zshrc .zshenv .vim .vimrc .vimrc.plugin .gvimrc .gitconfig .tmux.conf)
+DOT_FILES=(.zshrc .zshenv .vim .vimrc .vimrc.plugin .gvimrc .gitconfig .tmux.conf .gemrc)
 
 for file in ${DOT_FILES[@]}
 do
